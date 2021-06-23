99dcd56472e0759b4fce6f77c08635069ca7ef46
grahamjharvey <grahamjharvey@gmail.com>
2021-06-24 09:57:34 +1000

    Update PS_msiexec_remote_CBC_install.ps1

--- PS_msiexec_remote_CBC_install.ps1
+++ PS_msiexec_remote_CBC_install.ps1
@@ -2,6 +2,6 @@ $installer = "installer_vista_win7_win8-64-3.7.0.1253.msi"
 $computers = Get-Content -Path Computers.txt
 foreach ($pc in $computers) {
     Copy-Item -Path $installer -Destination "\\$pc\c$\Windows\Temp" -Force -Recurse
-    Invoke-Command -ComputerName $pc -ScriptBlock {Start-Process c:\windows\system32\msiexec.exe -ArgumentList '/q /i c:\windows\temp\installer_vista_win7_win8-64-3.7.0.1253.msi /L* c:\windows\temp\log.txt COMPANY_CODE=YNQ33DF7WAWY95DPAC7A CLI_USERS=S-1-5-18' -wait}
+    Invoke-Command -ComputerName $pc -ScriptBlock {Start-Process c:\windows\system32\msiexec.exe -ArgumentList '/q /i c:\windows\temp\installer_vista_win7_win8-64-3.7.0.1253.msi /L* c:\windows\temp\log.txt COMPANY_CODE=<INSERT YOUR COMPANY CODE HERE> CLI_USERS=<INSERT WINDOWS SID HERE>' -wait}
     Remove-Item -Path "\\$pc\c$\Windows\Temp\installer_vista_win7_win8-64-3.7.0.1253.msi" -Force
 }
